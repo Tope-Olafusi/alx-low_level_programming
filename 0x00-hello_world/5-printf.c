a7d1eb335a095971268c038302bd26ab332e9ab3
Tope0lafusi <topeola60@mail.com>
2023-02-16 14:03:17 -0800

    compilier

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-/*c program that runs printf function*/
-int main(void) {
-printf("with proper grammar, but the outcome is a piece of art,/n");
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
