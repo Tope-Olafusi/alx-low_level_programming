fe3c39f93dc918e58077270986aa23518d5af4d4
Tope0lafusi <topeola60@mail.com>
2023-02-24 16:55:48 -0800

    job

---
+++ 0-main.c
@@ -0,0 +1,18 @@
+able File  16 lines (12 sloc)  182 Bytes
+
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
