9da62d60ec3ec8622eb1e169af6080898e7943b8
Tope-Olafusi <topeola60@gmail.com>
2023-06-15 18:07:43 -0700

    a new beginning

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,19 +1,32 @@
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * *_calloc - call
- * Return: 0
+ * _memset - copy char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an ar
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *memory;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	memory = malloc(nmemb * size);
-	if (memory == NULL)
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
-		*(memory + i) = 0;
-	return ((void *)memory);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
