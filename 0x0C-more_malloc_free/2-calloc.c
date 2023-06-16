5c2cf0a966bccdab8bacb1f245038660019ad606
Tope-Olafusi <topeola60@gmail.com>
2023-06-15 17:09:59 -0700

    a new beginning

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,19 @@
-#include "main.h"
+#include <stdlib.h>
+/**
+ * *_calloc - call
+ * Return: 0
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
-	unsigned int b;
+	unsigned int i;
+	char *memory;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	a = malloc(nmemb * size);
-	if (a == NULL)
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
 		return (NULL);
-	for (b = 0; b < (nmemb * size); b++)
-		a[b] = 0;
-	return (a);
+	for (i = 0; i < nmemb * size; i++)
+		*(memory + i) = 0;
+	return ((void *)memory);
 }
