9fa61203a51786f2b2bccf512eddc670743c5710
Tope-Olafusi <topeola60@gmail.com>
2023-06-19 20:30:36 -0700

    God of wonders

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*This program print out the name of the folder it was compiled from using the predefined macro in c language*/
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
