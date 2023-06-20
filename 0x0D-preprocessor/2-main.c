e532f9e1475a8c0a34d527ebb0c32ebc053e9417
Tope-Olafusi <topeola60@gmail.com>
2023-06-20 15:54:04 -0700

    network

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,6 +2,6 @@
 /*This program print out the name of the folder it was compiled from using the predefined macro in c language*/
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("Compiled from file: %s\n", __BASE_FILE__);
 	return (0);
 }
